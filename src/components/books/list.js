3686887c71febe6a90cd848f63052cbb4e9e83a7
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-08-30 17:25:26 -0300

    Add and remove with Redux

--- src/components/books/list.js
+++ src/components/books/list.js
@@ -1,38 +1,23 @@
 /* eslint-disable no-unused-vars */
+import { useSelector } from 'react-redux';
 import React from 'react';
 import Books from './books.js';
 import Form from './form.js';
 import './list.css';
 
-class List extends React.Component {
-  state = {
-    bookList: [{
-      title: 'Republic',
-      author: 'Plato',
-      genre: 'Phylosophy',
-      id: 1,
-    },
-    {
-      title: 'The Prince',
-      author: 'Machiavel',
-      genre: 'Phylosophy',
-      id: 2,
-    }],
-  }
-
-  render() {
-    return (
+const List = () => {
+  const bookList = useSelector((state) => state.booksReducer);
+  return (
     <div>
         <ul>
-            {this.state.bookList.map((book) => (
-               <Books name={book.title} author={book.author} genre={book.genre}
+            {bookList.map((book) => (
+               <Books name={book.bookTitle} genre={book.bookCategory} id={book.id}
                key={book.id}/>
             ))}
         </ul>
           <Form />
     </div>
-    );
-  }
-}
+  );
+};
 
 export default List;
---
+++ src/redux/books/books.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+
+// ACTIONS types
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+
+// empty book array
+const initialState = [];
+
+// ACTION CREATOR
+// add
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+// remove
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+});
+
+// REDUCER
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
