36a94ba95a0a4533d18a52bf07ba5386f49675b4
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-09-01 17:21:26 -0300

    Api added
    -Api added so data can persist.
    -Use of middleware and thunks.
    -Axios was used instead of fetch.

--- src/components/books/list.js
+++ src/components/books/list.js
@@ -1,18 +1,25 @@
 /* eslint-disable no-unused-vars */
-import { useSelector } from 'react-redux';
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
 import Books from './books.js';
 import Form from './form.js';
 import './list.css';
+import { setBook } from '../../redux/books/books.js';
 
 const List = () => {
   const bookList = useSelector((state) => state.booksReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setBook());
+  }, []);
+
   return (
     <div>
         <ul>
             {bookList.map((book) => (
-               <Books name={book.bookTitle} genre={book.bookCategory} id={book.id}
-               key={book.id}/>
+               <Books name={book.title} genre={book.category} id={book.item_id}
+               key={book.item_id}/>
             ))}
         </ul>
           <Form />
